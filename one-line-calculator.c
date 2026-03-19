0b9af689d28b24f58cba640b65e2e3c82e4381f7
Satya Virya Atmadja <satyaatmadja07@gmail.com>
2026-03-19 09:09:50 +0700

    Add evaluation for one digit operation

--- one-line-calculator.c
+++ one-line-calculator.c
@@ -13,6 +13,16 @@ int isOperator(char symbol) {
     }
 }
 
+int evaluateOperator(int a, int b, char symbol) {
+	switch (symbol) {
+        case '+': return a+b;
+        case '-': return a-b;
+        case '*': return a*b;
+        case '/': return a/b;
+        case '^': return a^b;
+    }
+}
+
 void infixToPostfix(char* infix, char* postfix) {
     char stack[100];
 
@@ -24,22 +34,23 @@ void infixToPostfix(char* infix, char* postfix) {
         char c = infix[i];
 
         if (c == ' ') {
-            i++;
-            continue;
+//          postfix[++idx] = c; // supaya bisa bedain kalau angka lebih dari 1 digit
+			i++;
+			continue;
         }
 
-        if (c == '(') {
+        else if (c == '(') {
             stack[++top] = c;
         }
 
-        if (c == ')') {
+        else if (c == ')') {
             while (stack[top] != '(') {
                 postfix[++idx] = stack[top--];
             }
             top--; //hilangkan (
         }
 
-        if (isOperator(c) > 0) {
+        else if (isOperator(c) > 0) {
             while (isOperator(stack[top]) >= isOperator(c) && stack[top] != '(') {
                 postfix[++idx] = stack[top--];
             }
@@ -48,7 +59,7 @@ void infixToPostfix(char* infix, char* postfix) {
         
         }
 
-        if (isOperator(c) == 0 && c != '(' && c != ')'){
+        else if (isOperator(c) == 0 && c != '(' && c != ')'){
             postfix[++idx] = c;
         }
         
@@ -64,6 +75,53 @@ void infixToPostfix(char* infix, char* postfix) {
 
 }
 
+int evaluatePostfix(char* postfix) {
+	int topStack = -1;
+	int idxNum = 0;
+	int idx = 0;
+	
+	int stack[100] = {0};
+	char strNum[10]; //maksimal digit angka 10
+	
+	
+	while (postfix[idx] != '\0') {
+		char c = postfix[idx];
+//		printf("now %c\n", c);
+		
+		if ( c >= '0' && c <= '9') {
+//			strNum[idxNum++] = c;
+			int temp = c - '0';
+			stack[++topStack] = temp;
+			
+//			printf("stack now \n");
+//			int i;
+//			for (i = 0; i <= topStack; i++) {
+//				printf("%d ", stack[i]);
+//			}
+//			printf("\n");
+		}
+		
+//		else if (c == ' ') {
+//			stack[topStack++] = atoi(strNum);
+//			strNum[0] = '\0';
+//			idxNum = 0;
+//		}
+		
+		else if (isOperator(c) > 0) {
+			int b = stack[topStack--];
+			int a = stack[topStack--];
+//			printf("operator %d %d %c\n", a, b, c);
+			int result = evaluateOperator(a,b, c);
+			stack[++topStack] = result;
+		}
+		
+		
+		idx++;
+	}
+	
+	return stack[topStack];
+}
+
 void menu() {
     char buffer[100];
     printf("Masukan Angka dan Operator yang ingin dievaluasi [maks 100 char]: ");
@@ -72,13 +130,16 @@ void menu() {
     char* postfix = (char*)malloc(strlen(buffer) + 1);
     infixToPostfix(buffer, postfix);
     
-    printf("%s\n", buffer);
-    printf("%s\n", postfix);
+//    printf("%s\n", buffer);
+//    printf("%s\n", postfix);
+    
+    int resultFinal = evaluatePostfix(postfix);
+    printf("Final Result = %d\n", resultFinal);
     return;
 }
 
 
-int main() {
+int main() {	
 
     menu();
 
